a83d07e3c1c7a81f849e1d3cdf0966eb6fb36984
Lauryn Brown <lauryndbrown@gmail.com>
2018-06-13 12:36:36 -0700

    Added tests for refresh identity.

--- tests/sentry/integrations/vsts/test_integration.py
+++ tests/sentry/integrations/vsts/test_integration.py
@@ -2,9 +2,12 @@ from __future__ import absolute_import
 
 import responses
 
+import pytest
+
 from mock import Mock
 from django.http import HttpRequest
 
+from sentry.auth.exceptions import IdentityNotValid
 from sentry.integrations.vsts import VstsIntegration, VstsIntegrationProvider, ProjectConfigView, ProjectForm, get_projects
 from sentry.identity.vsts import VSTSIdentityProvider
 from sentry.models import Integration, Identity, IdentityProvider
@@ -65,7 +68,7 @@ class ProjectConfigViewTest(TestCase):
         assert pipeline.next_step.call_count == 1
 
 
-class VSTSIntegrationTest(TestCase):
+class VstsIntegrationProviderTest(TestCase):
     def setUp(self):
         self.integration = VstsIntegrationProvider()
 
@@ -99,30 +102,66 @@ class VSTSIntegrationTest(TestCase):
         assert integration_dict['user_identity']['data']['token_type'] == 'jwt-bearer'
 
 
-class VstsIntegrationTest(TestCase):
-    def test_get_client(self):
-        user = self.create_user()
-        organization = self.create_organization()
-        access_token = '1234567890'
-        model = Integration.objects.create(
+class VstsIntegrationTestCase(TestCase):
+
+    def setUp(self):
+        self.user = self.create_user()
+        self.organization = self.create_organization()
+        self.access_token = '1234567890'
+        self.model = Integration.objects.create(
             provider='integrations:vsts',
             external_id='vsts_external_id',
             name='vsts_name',
+            metadata={
+                'domain_name': 'sentryuser.visualstudio.com',
+            }
         )
-
-        identity = Identity.objects.create(
+        self.identity = Identity.objects.create(
             idp=IdentityProvider.objects.create(
                 type='vsts',
                 config={}
             ),
-            user=user,
+            user=self.user,
             external_id='vsts_id',
             data={
-                'access_token': access_token
+                'access_token': self.access_token
             }
         )
-        model.add_organization(organization.id, identity.id)
-        integration = VstsIntegration(model, organization.id)
-        client = integration.get_client()
+        self.model.add_organization(self.organization.id, self.identity.id)
+        self.integration = VstsIntegration(self.model, self.organization.id)
 
-        assert client.access_token == access_token
+    def test_get_client(self):
+        client = self.integration.get_client()
+        assert client.access_token == self.access_token
+
+    def test_get_refresh_identity_params_incomplete(self):
+        # no refresh token in identity
+        self.integration.default_identity = self.integration.get_default_identity()
+        with pytest.raises(IdentityNotValid):
+            self.integration.get_refresh_identity_params()
+
+    @responses.activate
+    def test_refresh_identity(self):
+        refresh_data = {
+            'access_token': 'access token for this user',
+            'token_type': 'type of token',
+            'expires_in': 'time in seconds that the token remains valid',
+            'refresh_token': 'new refresh token to use when the token has timed out',
+        }
+        responses.add(
+            responses.POST,
+            'https://app.vssps.visualstudio.com/oauth2/token',
+            json=refresh_data,
+        )
+        refresh_token = '123456789'
+        self.identity.update(
+            data={
+                'access_token': self.access_token,
+                'refresh_token': refresh_token,
+            }
+        )
+        self.integration.refresh_identity()
+
+        assert len(responses.calls) == 1
+        assert self.integration.default_identity.data == refresh_data
+        assert Identity.objects.get(id=self.identity.id).data == refresh_data
